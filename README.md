ebd73e363a6a8487f0b4971c30dc04f08f59c22d
Sanjiv Shinde <sanjiv.s@tezo.com>
2025-01-28 15:08:15 +0530

    HeroSection completed

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
+npm create vite@latest Company_Website
+npm install
+npm run dev
+
+
 # React + Vite
+
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import Navbar from './component/Navbar'
+import React from "react";
+import Navbar from "./component/Navbar";
+import HeroSection from "./component/HeroSection";
 
 const App = () => {
   return (
     <div>
-<Navbar></Navbar>
-   </div>
-  )
-}
+      <Navbar />
+      <div className="max-w-7xl max-auto pt-20 px-6">
+        <HeroSection />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
